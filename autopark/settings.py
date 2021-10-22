4a7d0730bb5972f53956dcd9c7105366660608c7
sheleh <shelex87@gmail.com>
2021-10-22 18:11:14 +0300

    database changed to POSTGRES with all data

--- autopark/settings.py
+++ autopark/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'autopark.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'autopark',
+        'USER': 'dmitriy',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
